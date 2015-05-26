201efa022d9c919e88921eb0d200feb72fd67da3
Christoph Willinger <christoph.willinger@gmail.com>
2015-05-26 10:36:49 +0200

    Better JS syntax

--- binary-clock.js
+++ binary-clock.js
@@ -1,49 +1,59 @@
 function BinaryClock(size, image_url, targetContainerId) {
-  this.s = size;
-  this.url = image_url;
-  this.container = targetContainerId;
-  this.start = start;
-  this.init = init;
+    this.size = size;
+    this.url = image_url;
+    this.container = targetContainerId;
+    this.interval = null;
+
+    this.start();
 }
 
-function update(bc) {
-  var time = new Date();
-  var seconds = time.getSeconds();
-  var minutes = time.getMinutes();
-  var hours = time.getHours();
-  
-  var dA, dB, dC, dD, dE, dF;
-  
-  dA = Math.floor(hours/10);
-  dB = hours - dA*10;
-  
-  dC = Math.floor(minutes/10);
-  dD = minutes - dC*10;
-  
-  dE = Math.floor(seconds/10);
-  dF = seconds - dE*10;
+BinaryClock.prototype.update = function() {
+    var time = new Date();
+    var seconds = time.getSeconds();
+    var minutes = time.getMinutes();
+    var hours = time.getHours();
 
-  document.getElementById(bc.container).childNodes.item(0).style.backgroundPosition = dA*(-bc.s)+"px 0px";
-  document.getElementById(bc.container).childNodes.item(1).style.backgroundPosition = dB*(-bc.s)+"px 0px";
-  document.getElementById(bc.container).childNodes.item(2).style.backgroundPosition = dC*(-bc.s)+"px 0px";
-  document.getElementById(bc.container).childNodes.item(3).style.backgroundPosition = dD*(-bc.s)+"px 0px";
-  document.getElementById(bc.container).childNodes.item(4).style.backgroundPosition = dE*(-bc.s)+"px 0px";
-  document.getElementById(bc.container).childNodes.item(5).style.backgroundPosition = dF*(-bc.s)+"px 0px";
+    var dA, dB, dC, dD, dE, dF;
+
+    dA = Math.floor(hours/10);
+    dB = hours - dA*10;
+
+    dC = Math.floor(minutes/10);
+    dD = minutes - dC*10;
+
+    dE = Math.floor(seconds/10);
+    dF = seconds - dE*10;
+
+    var element = document.getElementById(this.container);
+
+    element.childNodes.item(0).style.backgroundPosition = dA*(-this.size)+"px 0px";
+    element.childNodes.item(1).style.backgroundPosition = dB*(-this.size)+"px 0px";
+    element.childNodes.item(2).style.backgroundPosition = dC*(-this.size)+"px 0px";
+    element.childNodes.item(3).style.backgroundPosition = dD*(-this.size)+"px 0px";
+    element.childNodes.item(4).style.backgroundPosition = dE*(-this.size)+"px 0px";
+    element.childNodes.item(5).style.backgroundPosition = dF*(-this.size)+"px 0px";
 };
 
-function start() {
+BinaryClock.prototype.init = function() {
+    var html =
+        '<div id="a" style="width: '+this.size+'px; height: '+this.size*4+'px; background: url(\''+this.url+'\'); display: inline-block; background-repeat: no-repeat;"></div>' +
+        '<div id="b" style="width: '+this.size+'px; height: '+this.size*4+'px; background: url(\''+this.url+'\'); display: inline-block; background-repeat: no-repeat;"></div>' +
+        '<div id="c" style="width: '+this.size+'px; height: '+this.size*4+'px; background: url(\''+this.url+'\'); display: inline-block; background-repeat: no-repeat;"></div>' +
+        '<div id="d" style="width: '+this.size+'px; height: '+this.size*4+'px; background: url(\''+this.url+'\'); display: inline-block; background-repeat: no-repeat;"></div>' +
+        '<div id="e" style="width: '+this.size+'px; height: '+this.size*4+'px; background: url(\''+this.url+'\'); display: inline-block; background-repeat: no-repeat;"></div>' +
+        '<div id="f" style="width: '+this.size+'px; height: '+this.size*4+'px; background: url(\''+this.url+'\'); display: inline-block; background-repeat: no-repeat;"></div>';
+
+    document.getElementById(this.container).innerHTML = html;
+};
+
+BinaryClock.prototype.stop = function() {
+    if (this.interval) {
+        window.clearInterval(this.interval);
+    }
+};
+
+BinaryClock.prototype.start = function() {
     this.init();
-    window.setInterval(update, 500, this);
+    this.update();
+    this.interval = window.setInterval(this.update.bind(this), 1000);
 };
-
-function init() {
-  var html = 
-    '<div id="a" style="width: '+this.s+'px; height: '+this.s*4+'px; background: url(\''+this.url+'\'); display: inline-block; background-repeat: no-repeat;"></div>' +
-    '<div id="b" style="width: '+this.s+'px; height: '+this.s*4+'px; background: url(\''+this.url+'\'); display: inline-block; background-repeat: no-repeat;"></div>' +
-    '<div id="c" style="width: '+this.s+'px; height: '+this.s*4+'px; background: url(\''+this.url+'\'); display: inline-block; background-repeat: no-repeat;"></div>' +
-    '<div id="d" style="width: '+this.s+'px; height: '+this.s*4+'px; background: url(\''+this.url+'\'); display: inline-block; background-repeat: no-repeat;"></div>' +
-    '<div id="e" style="width: '+this.s+'px; height: '+this.s*4+'px; background: url(\''+this.url+'\'); display: inline-block; background-repeat: no-repeat;"></div>' +
-    '<div id="f" style="width: '+this.s+'px; height: '+this.s*4+'px; background: url(\''+this.url+'\'); display: inline-block; background-repeat: no-repeat;"></div>';
-  
-  document.getElementById(this.container).innerHTML = html;
-}
